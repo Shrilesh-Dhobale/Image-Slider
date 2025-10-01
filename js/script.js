c4b993d75d0759735a5e74ad2c654b263da6e365
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-01 18:26:16 +0530

    next fun created

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 const images=["car1.jpg","car2.jpg","car3.jpg"];
 let index=0;
 const slide=document.getElementById("slide");
+
+function next(){
+    if(index<images.length-1){
+        index++;
+    }
+    else{
+        index=0;
+    }
+    slide.src=images[index];
+
+    
+}
