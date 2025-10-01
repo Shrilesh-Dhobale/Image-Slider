f21b91321f3b3ee16404aac22279829e9408bc1d
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-10-01 18:35:46 +0530

    Prev fun created

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@ let index=0;
 const slide=document.getElementById("slide");
 
 function next(){
-    if(index<images.length-1){
+    if(index < images.length - 1){
         index++;
     }
     else{
-        index=0;
+        index = 0;
     }
-    slide.src=images[index];
+    slide.src = images[index];
+}
 
-    
+function prev(){
+    if(index > 0){
+        index--;
+    }
+    else{
+        index = images.length - 1;
+    }
+    slide.src = images[index];
 }
